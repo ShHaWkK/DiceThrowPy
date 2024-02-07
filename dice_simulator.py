d578d7c443f577d416c689eef4d5f0816ab4d29b
ShHaWkK <alexandreuzan9@gmail.com>
2024-02-07 15:06:03 +0100

    Update .py (import tkinter)

--- dice_simulator.py
+++ dice_simulator.py
@@ -1,18 +1,14 @@
+import tkinter as tk
 import random
 
-def roll_dice(num_sides=6):
-    """Fonction pour simuler le lancer d'un dé à 'num_sides' faces."""
-    return random.randint(1, num_sides)
 
-def main():
-    num_sides = input("Entrez le nombre de faces du dé (6 par défaut): ")
+def roll_dice():
+    num_sides = sides_var.get()
     if not num_sides:
         num_sides = 6
     else:
         num_sides = int(num_sides)
 
-    roll = roll_dice(num_sides)
-    print(f"Résultat du lancer de dé: {roll}")
+    roll = random.randint(1, num_sides)
+    result_label.config(text=f"Résultat : {roll}")
 
-if __name__ == "__main__":
-    main()
