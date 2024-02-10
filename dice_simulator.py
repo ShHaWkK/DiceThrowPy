cc9a982a5987d64322bc33d4dc15ad3112191a7d
Alexandre Uzan <51519814+ShHaWkK@users.noreply.github.com>
2024-02-10 22:17:21 +0100

    Update dice_simulator.py

--- dice_simulator.py
+++ dice_simulator.py
@@ -6,6 +6,7 @@ import time
 
 
 def roll_dice():
+    
     num_dices = int(num_dices_spinbox.get())
     results = [random.randint(1, 6) for _ in range(num_dices)]
     show_dice_animations(results)
@@ -14,7 +15,8 @@ def roll_dice():
 
 
 def show_dice_animations(results):
-    for _ in range(10):  # Nombre d'itérations pour l'animation
+    
+    for _ in range(10):  
         for label in dice_image_labels:
             label.config(image=random.choice(dice_images))
         app.update()
@@ -25,22 +27,26 @@ def show_dice_animations(results):
 
 
 def update_statistics(results):
+    
     total = sum(results)
     avg = total / len(results) if results else 0
     stats_label.config(text=f"Total: {total}, Moyenne: {avg:.2f}")
 
 
 def update_history(results):
+    
     history.insert(tk.END, f"{results} (Total: {sum(results)})\n")
     history.see(tk.END)
 
 
 def setup_dice_labels(num_dices):
+    
     for label in dice_image_labels:
         label.pack_forget()
     dice_image_labels.clear()
 
     for _ in range(num_dices):
+        
         label = tk.Label(dice_frame, image=dice_images[0])
         label.pack(side=tk.LEFT)
         dice_image_labels.append(label)
@@ -50,11 +56,11 @@ app = tk.Tk()
 app.title("Simulateur de Lancer de Dés Animé Avancé")
 
 style = ttk.Style(app)
-style.theme_use('clam')  # Choix d'un thème
+style.theme_use('clam') 
 
 num_dices_var = tk.StringVar(value='1')
 num_dices_spinbox = ttk.Spinbox(app, from_=1, to=6, textvariable=num_dices_var, width=5,
-                                command=lambda: setup_dice_labels(int(num_dices_spinbox.get())))
+command=lambda: setup_dice_labels(int(num_dices_spinbox.get())))
 num_dices_spinbox.pack()
 
 roll_button = ttk.Button(app, text="Lancer les dés", command=roll_dice)
